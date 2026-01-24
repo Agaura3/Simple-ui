f7d7ed566122bba6971ff0a5eb4f3ab8a591fc46
Your Name <you@email.com>
2026-01-24 05:58:59 +0530

    fix: angular app deploy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,38 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { environment } from '../environments/environment';
-
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   num1!: number;
   num2!: number;
-  message: string = '';
+  message = '';
 
   constructor(private http: HttpClient) {}
 
-checkResult() {
-  this.http.get<any>(
-    `https://simple-ui-xu8r.onrender.com/check?num1=${this.num1}&num2=${this.num2}`
-  ).subscribe(res => {
-    if (res.status === 'greater') {
-      this.message = 'First number is greater than second number';
-    } else if (res.status === 'less') {
-      this.message = 'First number is less than second number';
-    } else {
-      this.message = 'Both numbers are equal';
-    }
-  });
+  checkResult() {
+    this.http.get<any>(
+      `https://simple-ui-xu8r.onrender.com/check?num1=${this.num1}&num2=${this.num2}`
+    ).subscribe({
+      next: (res) => {
+        if (res.status === 'greater') {
+          this.message = 'First number is greater than second number';
+        } else if (res.status === 'less') {
+          this.message = 'First number is less than second number';
+        } else {
+          this.message = 'Both numbers are equal';
+        }
+      },
+      error: () => {
+        this.message = 'Backend error / Render sleeping';
+      }
+    });
+  }
 }
-}
-
-    
